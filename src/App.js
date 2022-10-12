e2aef87996fd8a5a8941a385d762ed82a2fc4a30
enjirtmnv <shkensinio@gmail.com>
2022-10-12 19:39:24 +0300

    fix fetch entered request

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ function App() {
     try {
       const getInitialData = async () => {
         const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}`)
+
         const data = await res.json()
         setAddress(data)
         setDomain(data.as.domain)
@@ -37,14 +38,20 @@ function App() {
       getInitialData();
 
     } catch(e)  {
-      console.trace(error)
+      console.trace(e)
     }
   }, [])
 
   const getEnteredAddress = async () => {
     const res = await fetch(
       `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_API_KEY}&${checkInput(ipAddress)}`)
-      const data = await res.json()
+    
+    if (!res.ok) {
+      const message = `An error occured: ${res.status}`
+      throw new Error(message)
+    }
+    
+    const data = await res.json()
     setAddress(data)
     setDomain(data.as.domain)
   }
